14baf7220f27c901c80e5d827cdbd1c589930adb
zoe-k <5705621+zoe-k@users.noreply.github.com>
2024-10-05 21:08:37 +0100

    deleted stuff not needed

--- src/enemy.gd
+++ src/enemy.gd
@@ -10,7 +10,7 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.RIGHT.rotated(direction) * SPEED
 	move_and_slide()
-	
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
