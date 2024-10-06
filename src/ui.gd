c5382057c9deca2c752772bb10a986b8c8f68a80
Varvill <jamescook1999@gmail.com>
2024-10-06 20:26:13 +0100

    score

--- src/ui.gd
+++ src/ui.gd
@@ -3,15 +3,15 @@ extends Control
 @export var _game: Node
 
 var stageScene = preload("res://src/stage.tscn")
+var score = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	SignalBus.enemy_hit.connect(on_enemy_hit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func start():
 	if not _game.get_children().is_empty():
@@ -24,6 +24,7 @@ func start():
 	$Panel.hide()
 	$Panel2.hide()
 	$Panel/AudioStreamPlayer.stop()
+	$RichTextLabel.show()
 
 
 
@@ -31,9 +32,15 @@ func _on_game_over(FinishState):
 	print("FUCK!!!!!")
 	$Panel2.show()
 
+func on_enemy_hit(enemy_hit):
+	score += 1
+	$RichTextLabel.clear()
+	$RichTextLabel.add_text("Score:" + str(score))
+
 func quit():
 	get_tree().quit()
 
 
 func restart():
+	$RichTextLabel.clear()
 	start()
