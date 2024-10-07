318b14e4c9a91ba4fac057f4727e68f6bf8ecbcf
John Stones <john.stones@protonmail.com>
2024-10-07 17:27:39 +0100

    Add health value to bar

--- src/health_bar.gd
+++ src/health_bar.gd
@@ -1,10 +1,14 @@
 class_name HealthBar extends ProgressBar
 
 
+@onready var _label := $HealthValue as Label
+
+
 func _ready():
 	SignalBus.player_health_changed.connect(update_value)
 
 
 func update_value(new_value: int):
 	value = new_value
+	_label.text = str(new_value) + " / 100"
 
