8744fbb990b78a1c0116243b1f979a8e85483f2a
zoe-k <5705621+zoe-k@users.noreply.github.com>
2024-10-07 13:49:34 +0100

    minor adjustment to following enemy stats

--- src/following_enemy.gd
+++ src/following_enemy.gd
@@ -4,15 +4,15 @@ var _player: Player
 
 func _ready() -> void:
 	speed = 100
-	damage = 3
-	health_bonus = 5
+	damage = 4
+	health_bonus = 3
 
 func _physics_process(delta: float) -> void:
 	var distance_to_player = position.distance_to(_player.position)
 	if distance_to_player > 0 && distance_to_player < 50 :
 		direction += randf_range(-PI / 4, PI / 4)
 		velocity = Vector2.RIGHT.rotated(direction) * speed
-	else:
+	else: 
 		velocity = position.direction_to(_player.position) * speed
 		look_at(_player.position)
 	move_and_slide()
